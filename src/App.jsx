067144a04ded38a975128a0d8364fa9cb35323b8
Sallytion <Yash.tekwani2022@vitstudent.ac.in>
2024-09-07 19:14:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { AnimatedPinDemo } from "./components/AnimatedPin";
+
+function App() {
+
+  return (
+    <>
+      <Button>Hello</Button>
+      <AnimatedPinDemo></AnimatedPinDemo>
+    </>
+  );
+}
+
+export default App;
